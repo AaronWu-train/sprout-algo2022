4e2028ac695f53679b1267a2309e818f7a63ca6e
AaronWu-train <aaronwu0218@gmail.com>
2022-03-12 14:51:24 +0800

    20220312-0251

---
+++ 0312/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  0312
+//
+//  Created by Aaron Wu on 2022/3/12.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
