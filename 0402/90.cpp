7575fb09adfc4679c173704e9dd2a57ec999b68f
AaronWu-train <aaronwu0218@gmail.com>
2022-04-09 19:45:43 +0800

    Local Changes

---
+++ 0402/90.cpp
@@ -0,0 +1,8 @@
+//
+//  90.cpp
+//  0402
+//
+//  Created by Aaron Wu on 2022/4/6.
+//
+
+#include <stdio.h>
---
+++ 0409/124.cpp
@@ -0,0 +1,17 @@
+#pragma GCC optimize("Ofast")
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <climits>
+#include <bitset>
+#define All(x) begin(x),end(x)
+#define ll long long int
+#define debug(x) cerr<<#x<<" = "<<(x)<<endl
+#define _ ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+using namespace std;
+
+signed main() {_
+    
+    return 0;
+}
