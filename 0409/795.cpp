bf0fad6b82917125108ac7794a3cf94cac0cab76
AaronWu-train <aaronwu0218@gmail.com>
2022-04-14 20:16:23 +0800

    local changes

--- 0409/795.cpp
+++ 0409/795.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <algorithm>
 #include <climits>
+#include <cmath>
 #include <bitset>
 #define All(x) begin(x),end(x)
 #define ll long long int
@@ -19,30 +20,23 @@ inline ll dis(pair<ll, ll>a,pair<ll, ll>b){
     ll t =(b.x-a.x)*(b.x-a.x)+(b.y-a.y)*(b.y-a.y);
     return t;
 }
-
 ll fun(ll l, ll r,vector<pair<ll, ll>>&arr) {
     if (r<=l) {
-        return LLONG_MAX;
+        return 9e18;
     }
     ll mid = (r+l)/2;
-//    ll midline = arr[mid].x;
     ll d = min(fun(mid+1,r,arr), fun(l, mid,arr));
+    ll foundd = sqrt(d);
     vector<pair<ll, ll>>nearmid;
-    for (ll i = mid; i>=l&& arr[mid].x - arr[i].x <= d; --i) {
+    for (ll i = mid; i>=l&& arr[mid].x - arr[i].x <= foundd; --i) {
         nearmid.push_back(arr[i]);
     }
-    for (ll i=mid+1; i<=r && arr[i].x - arr[mid].x <= d; ++i){
+    for (ll i=mid+1; i<=r && arr[i].x - arr[mid].x <= foundd; ++i){
         nearmid.push_back(arr[i]);
     }
-//    for (int i = l; i <= r; ++i) {
-//        if (abs(midline-arr[i].x)<=d) {
-//            nearmid.push_back(arr[i]);
-//        }
-//    }
     sort(All(nearmid), [](auto a, auto b){
         return a.y < b.y;
     });
-//    print(nearmid);
     for (ll i = 0; i < nearmid.size(); ++i) {
         for (ll j = i+1; j < min((ll)i+3,(ll)nearmid.size()); ++j) {
             d = min(d, dis(nearmid[i],nearmid[j]));
@@ -59,11 +53,7 @@ signed main() {_
         cin >> arr[i].first >> arr[i].second;
     }
     sort(All(arr));
-    if (n==1) {
-        cout << 0 <<endl;
-    } else {
-        auto t =  fun(0,n-1,arr);
-        cout << min(t,dis(arr[0], arr[1])) << endl;
-    }
+    auto t =  fun(0,n-1,arr);
+    cout << t << endl;
     return 0;
 }
